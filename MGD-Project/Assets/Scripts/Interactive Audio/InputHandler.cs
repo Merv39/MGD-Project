a0d52f559b6e3447a81c2e0bfc096bbbc5b380de
Merv39 <78385901+Merv39@users.noreply.github.com>
2023-01-22 04:44:40 +0000

    print coordinates

--- MGD-Project/Assets/Scripts/Interactive Audio/InputHandler.cs
+++ MGD-Project/Assets/Scripts/Interactive Audio/InputHandler.cs
@@ -3,15 +3,25 @@ using UnityEngine.InputSystem;
 
 public class InputHandler : MonoBehaviour
 {
-    // Y Axis on MIDI CC1
-    void OnActivationAxis(InputValue value)
-    {
-        print("Activation: "+value.Get<float>());
-    }
+    float pleasant;
+    float activation;
 
-    // X Axis on MIDI CC2
+    // X Axis on MIDI CC1
     void OnPleasantAxis(InputValue value)
     {
-        print("Pleasant: "+value.Get<float>());
+        //print("Pleasant: "+value.Get<float>());
+        pleasant = value.Get<float>();
+    }
+
+    // Y Axis on MIDI CC2
+    void OnActivationAxis(InputValue value)
+    {
+        //print("Activation: " + value.Get<float>());
+        activation = value.Get<float>();
+    }
+
+    private void Update()
+    {
+        print((pleasant, activation));
     }
 }
